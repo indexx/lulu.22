61be6bf01eea31a39ecbd173145171b9e8596967
lulunasser <41142396+lulunasser@users.noreply.github.com>
2018-07-12 10:48:33 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,36 @@
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+   
+   
+   
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
+
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'firefox-icon.png.png') {
+      myImage.setAttribute ('src','firefox2.png');
+    } else {
+      myImage.setAttribute ('src','firefox-icon.png.png');
+    }
+}
+
+
